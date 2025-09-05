4586a423e63821f8a74c431597117442a5db0d8d
Lokesh Parab <66030300+lokeshparab@users.noreply.github.com>
2025-09-05 18:21:49 +0530

    Updated settings

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="bajaj-insurance-rag-based-chatbot",
+    version="0.0.1",
+    author="lokeshparab",
+    author_email="workingwithlokesh@gmail.com",
+    packages=find_packages(),
+)
